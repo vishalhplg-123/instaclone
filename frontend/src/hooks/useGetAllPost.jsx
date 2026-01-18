a5e304630e07bf71c0dc2139dc206ff1a5b67bab
vishal kumar <vishalkumarhplg@gmail.com>
2026-01-18 22:45:07 +0530

    fix postSlice import case issue

--- frontend/src/hooks/useGetAllPost.jsx
+++ frontend/src/hooks/useGetAllPost.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {useEffect} from "react"
 import { useDispatch } from "react-redux";
-import {setPosts}  from '@/redux/postslice'
+import {setPosts}  from '@/redux/postSlice'
 
 
 
